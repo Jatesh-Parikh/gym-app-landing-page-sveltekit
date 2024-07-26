a92b9cd3f9bbc013e0c19f2518c2d680d09bde60
Jatesh Parikh <jateshparikh97@gmail.com>
2024-07-26 19:49:03 +0530

    header and footer section done

---
+++ src/components/Footer.svelte
@@ -0,0 +1,32 @@
+<footer class="py-16 sm:py-20 md:py-24 px-4 md:px-8">
+    <div class="max-w-[1200px] mx-auto w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-8 text-base">
+        <div class="flex flex-col gap-4 md:col-span-2">
+            <h1 class="font-semibold">Swoley <span class="text-indigo-400">Moley</span> </h1>
+            <p>&copy; 2024 SWOLEY MOLEY Ltd. All rights reserved.</p>
+        </div>
+        <div class="flex flex-col gap-4">
+            <p class="font-bold poppins text-base sm:text-lg">Support</p>
+            <!-- svelte-ignore a11y-invalid-attribute -->
+            <a href="#" target="_blank" class="cursor-pointer duration-200 hover:text-indigo-400">Contact Us</a>
+        </div>
+        <div class="flex flex-col gap-4">
+                <p class="font-bold poppins text-base sm:text-lg">Research</p>
+                <!-- svelte-ignore a11y-invalid-attribute -->
+                <a href="#" target="_blank" class="cursor-pointer duration-200 hover:text-indigo-400">Read the Science</a>
+        </div>
+        <div class="flex flex-col gap-4">
+            <p class="font-bold poppins text-base sm:text-lg">Follow Us</p>
+            <!-- svelte-ignore a11y-invalid-attribute -->
+            <a href="#" target="_blank" class="cursor-pointer duration-200 hover:text-indigo-400"><i class="fa-brands fa-instagram pr-2"></i>Instagram</a>
+            <!-- svelte-ignore a11y-invalid-attribute -->
+            <a href="#" target="_blank" class="cursor-pointer duration-200 hover:text-indigo-400"><i class="fa-brands fa-youtube pr-2"></i>Youtube</a>
+            <!-- svelte-ignore a11y-invalid-attribute -->
+            <!-- svelte-ignore a11y-invalid-attribute -->
+            <a href="#" target="_blank" class="cursor-pointer duration-200 hover:text-indigo-400"><i class="fa-brands fa-facebook pr-2"></i>Facebook</a>
+            <!-- svelte-ignore a11y-invalid-attribute -->
+            <a href="#" target="_blank" class="cursor-pointer duration-200 hover:text-indigo-400"><i class="fa-brands fa-twitter pr-2"></i>Twitter</a>
+
+        </div>
+    </div>
+
+</footer>
