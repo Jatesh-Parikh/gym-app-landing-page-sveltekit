ed398a9ba56a34eff7238ef28a6077d75d280579
Jatesh Parikh <jateshparikh97@gmail.com>
2024-07-27 08:48:39 +0530

    faqs and conversion section done

---
+++ src/components/Conversion.svelte
@@ -0,0 +1,10 @@
+<script>
+    import CtAs from "./CTAs.svelte";
+</script>
+
+<section class="py-14 md:py-20 flex flex-col items-center justify-center gap-8 bg-[#181b34] text-white text-center">
+
+    <h4 class="text-lg sm:text-xl md:text-2xl poppins">Reach your fitness <span class="poppins font-bold text-indigo-400">goals and aspirations</span></h4>
+    <p class="text-base sm:text-lg md:text-xl"> ✦ Unlimited time on Free plan ✦</p>
+    <CtAs dark={true} />
+</section>
